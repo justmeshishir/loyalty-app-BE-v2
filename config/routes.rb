af8d831cf1ba812eccdf4ce99823328ddc54c317
justmeshishir <shishirthapa1@gmail.com>
2025-12-10 10:45:04 +0545

    scope customers route inside businessess

--- config/routes.rb
+++ config/routes.rb
@@ -16,8 +16,9 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :businessess
-      resources :customers
+      resources :businessess do
+        resources :customers
+      end
     end
   end
 end
