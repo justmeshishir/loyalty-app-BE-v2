aaa74168fde655c9e6a5304ea9ada3baf551adcf
justmeshishir <shishirthapa1@gmail.com>
2025-12-10 10:28:52 +0545

    business_customers relation mapping in customers

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,6 @@
 class Customer < ApplicationRecord
-  has_many :businesses
+  has_many :business_customers, dependent: :destroy
+  has_many :businesses, through: :business_customers
 
   enum :gender, { male: 0, female: 1, other: 2 }
 end
