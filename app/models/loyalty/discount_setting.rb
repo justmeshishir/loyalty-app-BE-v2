854236c33fdb2bd1be83b01199a61598b5dcedcd
justmeshishir <shishirthapa1@gmail.com>
2025-12-12 13:32:56 +0545

    json schema & validation for each loyalty settings

--- app/models/loyalty/discount_setting.rb
+++ app/models/loyalty/discount_setting.rb
@@ -1,5 +1,7 @@
 module Loyalty
   class DiscountSetting < LoyaltySetting
+    DISCOUNT_JSON_SCHEMA = Rails.root.join("config", "schemas", "discount_setting.json")
 
+    validates :rules, presence: true, json: { schema: DISCOUNT_JSON_SCHEMA }
   end
 end
--- app/models/loyalty/stamp_setting.rb
+++ app/models/loyalty/stamp_setting.rb
@@ -1,5 +1,7 @@
 module Loyalty
   class StampSetting < LoyaltySetting
+    STAMP_JSON_SCHEMA = Rails.root.join("config", "schemas", "stamp_setting.json")
 
+    validates :rules, presence: true, json: { schema: STAMP_JSON_SCHEMA }
   end
 end
