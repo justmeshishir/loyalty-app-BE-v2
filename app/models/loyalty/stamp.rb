5944e064074183b0cfc380042c8dd568ab30fc58
justmeshishir <shishirthapa1@gmail.com>
2025-12-16 18:00:08 +0545

    stamp model

---
+++ app/models/loyalty/stamp.rb
@@ -0,0 +1,3 @@
+class Loyalty::Stamp < ApplicationRecord
+  belongs_to :business_customer
+end
---
+++ db/migrate/20251216115522_create_loyalty_stamps.rb
@@ -0,0 +1,8 @@
+class CreateLoyaltyStamps < ActiveRecord::Migration[7.2]
+  def change
+    create_table :loyalty_stamps do |t|
+      t.references :business_customer, foreign_key: true, index: true
+      t.timestamps
+    end
+  end
+end
